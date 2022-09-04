28b30286c2729f5731c6069804847c4daa7a3d36
Victor Pinedo <victorpinedomartinez@gmail.com>
2022-09-04 16:46:41 -0500

    [add] Conteo de tareas pendientes

--- src/app/todos/components/todo-footer/todo-footer.component.ts
+++ src/app/todos/components/todo-footer/todo-footer.component.ts
@@ -11,12 +11,17 @@ import * as actions from '../../../filtro/filtro.actions';
 export class TodoFooterComponent implements OnInit {
   filtroActual: actions.filtrosValidos = "todos";
   filtros: actions.filtrosValidos[] = ["todos", "completados", "pendientes"];
+  pendientes: number = 0;
 
   constructor(private store: Store<AppState>) { }
 
   ngOnInit(): void {
-    this.store.select("filtro").subscribe(filtro => {
-      this.filtroActual = filtro;
+    // this.store.select("filtro").subscribe(filtro => {
+    //   this.filtroActual = filtro;
+    // });
+    this.store.subscribe(state => {
+      this.filtroActual = state.filtro;
+      this.pendientes = state.todos.filter(todo => !todo.isCompleted).length;
     });
   } 
 
